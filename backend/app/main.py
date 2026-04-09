8d0d24f0e95843222731dc8223f451d28b9b2ebd
Yusuf_azel <mdyusufali06122004@gmail.com>
2026-04-09 05:41:22 +0530

    fix: update frontend root and api url

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,10 @@ Base.metadata.create_all(bind=engine)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
